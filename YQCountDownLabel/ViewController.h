e9775b8a7c492825e65de2b8f8b7809be8bd2432
QuinceyYang <yqing555@qq.com>
2019-06-13 16:41:53 +0800

    Initial Commit

---
+++ YQCountDownLabel/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  YQCountDownLabel
+//
+//  Created by 杨清 on 2019/6/13.
+//  Copyright © 2019 QuinceyYang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ YQCountDownLabel/ViewController.m
@@ -0,0 +1,23 @@
+//
+//  ViewController.m
+//  YQCountDownLabel
+//
+//  Created by 杨清 on 2019/6/13.
+//  Copyright © 2019 QuinceyYang. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+@end
